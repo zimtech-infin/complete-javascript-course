a5281caa76e2396305957162bd3e9bfd09542bf8
Kelvin Zimmerman <kelvin@zimtechllc.us>
2024-03-16 17:09:12 -0400

    updates

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,103 @@
+/*
+
+The `let` keyword in JavaScript is used to declare a variable. In the provided code, `let js = 'amazing';` declares a variable named `js` and assigns it the string value 'amazing'. This variable can be used later in the code to refer to the value 'amazing'.
+
+*/
+      let js = 'amazing';
+
+
+
+
+      //prints the value of the math expression to the console
+      console.log(40 + 8 + 23 - 10);
+
+
+
+    
+      //prints the string  kelvin to the console
+      console.log("Kelvin");
+
+
+
+
+
+//this created variable is stored in the computers  memory  and can be used later in the code
+    let firstName = "Kelvin";
+
+
+
+
+
+//here we are passing the variable name in this function and it will print the value of the variable to the console-multiple times. Also variable should be written using camelcase 
+    console.log(firstName);
+    console.log(firstName);
+    console.log(firstName);
+    console.log(firstName);
+/*
+
+The `let` keyword is used to declare a variable in JavaScript. In the provided code, `let firstName = "Kelvin";` declares a variable named `firstName` and assigns it the string value 'Kelvin'. This variable can be used later in the code to refer to the value 'Kelvin'.
+
+*/
+
+
+//this code is correct because the variable name is written in camelcase an also the variable name is descriptive
+
+/*
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+*/
+
+
+
+//this code is not correct because the variable name is not written in camelcase
+//and also the variable name is not descriptive
+
+/*
+let job1 = 'programmer';
+let job2 = 'teacher';  
+*/
+
+
+
+//Objects and primitives in javascript: 
+
+
+/*
+
+Primitive data types: Number, String, Boolean, Undefined, Null, Symbol, BigInt
+Number: let age = 25;
+String: let name = "John";
+Boolean: let isStudent = true;
+Undefined: let address;
+Null: let car = null;
+Symbol: let id = Symbol("unique");
+BigInt: let bigNumber = 9007199254740991n;
+
+These examples demonstrate the basic usage of each data type. It's important to note that JavaScript is a dynamically typed language, which means that variables can hold different types of values at different times.
+
+*/
+
+
+/*
+
+Objects: Object literal, Arrays, Functions
+
+// Objects: Object literal, Arrays, Functions
+
+// Object literal
+let person = {
+    name: 'John',
+    age: 25,
+    isStudent: true
+};
+
+// Arrays
+let fruits = ['apple', 'banana', 'orange'];
+
+// Functions
+function sayHello() {
+    console.log('Hello!');
+}
+
+
+*/
