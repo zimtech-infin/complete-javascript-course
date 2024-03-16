bfb3671efc9391294f9afd7bd85e7fa71fbdb307
Kelvin Zimmerman <kelvin@zimtechllc.us>
2024-03-16 18:30:12 -0400

    updates

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,36 +3,38 @@
 The `let` keyword in JavaScript is used to declare a variable. In the provided code, `let js = 'amazing';` declares a variable named `js` and assigns it the string value 'amazing'. This variable can be used later in the code to refer to the value 'amazing'.
 
 */
-      let js = 'amazing';
+   
+
+//let js = 'amazing';
 
 
 
 
       //prints the value of the math expression to the console
-      console.log(40 + 8 + 23 - 10);
+     // console.log(40 + 8 + 23 - 10);
 
 
 
     
       //prints the string  kelvin to the console
-      console.log("Kelvin");
+      //console.log("Kelvin");
 
 
 
 
 
 //this created variable is stored in the computers  memory  and can be used later in the code
-    let firstName = "Kelvin";
+    //let firstName = "Kelvin";
 
 
 
 
 
 //here we are passing the variable name in this function and it will print the value of the variable to the console-multiple times. Also variable should be written using camelcase 
-    console.log(firstName);
-    console.log(firstName);
-    console.log(firstName);
-    console.log(firstName);
+    //console.log(firstName);
+   // console.log(firstName);
+    //console.log(firstName);
+    //console.log(firstName);
 /*
 
 The `let` keyword is used to declare a variable in JavaScript. In the provided code, `let firstName = "Kelvin";` declares a variable named `firstName` and assigns it the string value 'Kelvin'. This variable can be used later in the code to refer to the value 'Kelvin'.
@@ -98,6 +100,12 @@ let fruits = ['apple', 'banana', 'orange'];
 function sayHello() {
     console.log('Hello!');
 }
-
-
 */
+
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 'Kelvin');
